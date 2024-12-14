308d8eff0aa5b5b5cd79aa5a7dbaceac48e34685
Ivan-PS <51946898+Ivan-PS@users.noreply.github.com>
2024-12-14 15:47:06 +0100

    Inital commit

---
+++ android/app/src/main/kotlin/com/example/fibro_pred/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fibro_pred
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:fibro_pred/Activities/LoginActivity.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, // Hides the debug banner
+
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
+        ),
+        home: Loginactivity());
+  }
+}
